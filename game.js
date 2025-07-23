1208261ff3a35b9152db524d54a471796e58aa8d
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-07-23 18:25:21 +0800

    Add drawing functions for rectangle, circle, and text in game.js

--- game.js
+++ game.js
@@ -1,2 +1,21 @@
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext("2d");
+
+function drawRect(x, y, w, h, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, w, h);
+}
+
+function drawCircle(x, y, r, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawText(text, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.font = "45px Arial";
+  ctx.fillText(text, x, y);
+}
