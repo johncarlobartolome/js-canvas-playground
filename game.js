7aa598e8d163e1b7964c6bdb0bc0b904819040b3
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-07-23 18:29:59 +0800

    Add player, computer, ball, and net objects for Pong game

--- game.js
+++ game.js
@@ -1,5 +1,43 @@
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext("2d");
+const paddleWidth = 10,
+  paddleHeight = 100;
+
+const player = {
+  x: 0,
+  y: canvas.height / 2 - paddleHeight / 2,
+  width: paddleWidth,
+  height: paddleHeight,
+  color: "white",
+  score: 0,
+};
+
+const computer = {
+  x: canvas.width - paddleWidth,
+  y: canvas.height / 2 - paddleHeight / 2,
+  width: paddleWidth,
+  height: paddleHeight,
+  color: "white",
+  score: 0,
+};
+
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  radius: 10,
+  speed: 7,
+  velocityX: 5,
+  velocityY: 5,
+  color: "white",
+};
+
+const net = {
+  x: canvas.width / 2 - 1,
+  y: 0,
+  width: 2,
+  height: 10,
+  color: "white",
+};
 
 function drawRect(x, y, w, h, color) {
   ctx.fillStyle = color;
