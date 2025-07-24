384351a004cae2473b3a6f2494bc1971c24ef47c
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-07-24 13:49:58 +0800

    Implement Tetris player piece creation and rendering

--- tetris/game.js
+++ tetris/game.js
@@ -2,12 +2,35 @@ const canvas = document.getElementById("tetris");
 const ctx = canvas.getContext("2d");
 ctx.scale(30, 30);
 
+const player = {
+  pos: { x: 3, y: 0 },
+  matrix: createPiece_T(),
+};
+
+function createPiece_T() {
+  return [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ];
+}
+
+function drawMatrix(matrix, offset) {
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        ctx.fillStyle = "purple";
+        ctx.fillRect(x + offset.x, y + offset.y, 1, 1);
+      }
+    });
+  });
+}
+
 function draw() {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = "red";
-  ctx.fillRect(5, 5, 1, 1);
+  drawMatrix(player.matrix, player.pos);
 }
 
 draw();
